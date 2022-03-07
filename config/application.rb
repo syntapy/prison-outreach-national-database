3ad095307515f9bfc26173307f9ef2388b37b239
Chris <cdowney@gmail.com>
2022-03-07 03:35:12 -0500

    Force SSL only in prod

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,8 @@ module PrisonApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.force_ssl = true
-
+    config.force_ssl = true if Rails.env.production?
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
